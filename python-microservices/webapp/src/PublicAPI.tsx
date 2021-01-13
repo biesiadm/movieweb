9cb0b0efbdc264b3543602aec980e954f3f9bb63
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-01-13 03:11:05 +0100

    Remove TODO

--- python-microservices/webapp/src/PublicAPI.tsx
+++ python-microservices/webapp/src/PublicAPI.tsx
@@ -6,7 +6,6 @@ const axiosInstance: AxiosInstance = axios.create({
   responseType: "json"
 });
 
-// TODO(kantoniak): Use public API once we have it
 export default DefaultApiFactory(
   new Configuration(),
   process.env.REACT_APP_API_URL,
