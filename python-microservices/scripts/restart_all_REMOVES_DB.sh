207d01a5a3d728a6fdad300a258572071d8c6867
Mateusz Biesiadowski <mateusz.biesiadowski@gmail.com>
2021-01-15 22:38:44 +0100

    Added script

--- python-microservices/scripts/restart_all_REMOVES_DB.sh
+++ python-microservices/scripts/restart_all_REMOVES_DB.sh
@@ -6,21 +6,6 @@ set -e
 docker-compose down -v --remove-orphans
 
 ./user-service/scripts/restart-user.sh
+./movie-service/scripts/restart-movie.sh
 
 docker-compose up
-#
-## shut down containers and remove volumes
-#docker-compose down -v --remove-orphans
-#
-## remove old migrations
-#find ./movie-service/app/alembic/versions -type f -name '*.py' -delete
-#
-#docker-compose build
-#
-## run database and wait for it to start
-#docker-compose up -d movie-db
-#sleep 5
-#
-## create new revision
-#docker-compose run movie-service alembic revision --autogenerate
-#docker-compose up
