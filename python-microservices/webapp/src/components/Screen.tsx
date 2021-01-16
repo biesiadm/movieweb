1290336cc2b16f1034bd0d4f3e5cc5e04a43ff65
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-01-16 18:57:25 +0100

    Implement movie details view (#7)
    * Extract webapp components
    * Add public API methods
    * Implement webapp views

---
+++ python-microservices/webapp/src/components/Screen.tsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+type InfoScreenProps = {
+  className: string,
+  children: JSX.Element[]
+}
+type EmptyState = Record<string, never>
+
+class InfoScreen extends Component<InfoScreenProps, EmptyState> {
+
+  render(): React.ReactNode {
+    return <div className={`d-flex justify-content-center align-items-center py-5 text-secondary ${this.props.className}`}>
+            {this.props.children}
+          </div>;
+  }
+}
+
+type LoadingScreenProps = {
+  className: string,
+  message: string
+}
+
+class LoadingScreen extends Component<LoadingScreenProps, EmptyState> {
+
+  public static defaultProps = {
+    className: "",
+    message: "Loading..."
+  };
+
+  render(): React.ReactNode {
+    const message = this.props.message;
+    return <InfoScreen className={this.props.className}>
+            <div className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></div>
+            <p className="mb-0">{message}</p>
+          </InfoScreen>;
+  }
+}
+
+type ErrorScreenCallback = {
+  label: string,
+  callback: () => void
+}
+
+type ErrorScreenProps = {
+  className: string,
+  message: string,
+  callback?: ErrorScreenCallback
+}
+
+class ErrorScreen extends Component<ErrorScreenProps, EmptyState> {
+
+  public static defaultProps = {
+    className: "",
+    message: "Error",
+    callback: undefined
+  };
+
+  render(): React.ReactNode {
+    const message = this.props.message;
+
+    let button = <></>;
+    if (this.props.callback) {
+      const button_label = this.props.callback.label;
+      const callback = this.props.callback.callback;
+      button = <button onClick={callback} type="button" className="btn btn-secondary btn-xs mt-2">{button_label}</button>;
+    }
+
+    return <InfoScreen className={`flex-column ${this.props.className}`}>
+            <p className="small mb-0">{message}</p>
+            {button}
+          </InfoScreen>;
+  }
+}
+
+export default InfoScreen;
+export { LoadingScreen, ErrorScreen };
---
+++ python-microservices/webapp/src/pages/ErrorPage.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+interface Props {
+  header: string,
+  message: string
+}
+
+type EmptyState = Record<string, never>
+
+class ErrorPage extends Component<Props, EmptyState> {
+
+  public static defaultProps = {
+    header: "Oops!",
+    message: "There has been an error."
+  };
+
+  render(): React.ReactNode {
+    const history = this.props.history;
+    const header = this.props.header;
+    const message = this.props.message;
+    return <div className='text-center'>
+            <h1 className="display-1">{header}</h1>
+            <p className="lead">{message}</p>
+            <button onClick={history.goBack} type="button" className="btn btn-primary mt-3 py-2 px-3">Go back</button>
+          </div>
+  }
+}
+
+withRouter(ErrorPage);
+
+class GenericNotFoundPage extends ErrorPage {
+  public static defaultProps = {
+    header: "404",
+    message: "We couldn't find what you're looking for."
+  };
+}
+
+export default ErrorPage;
+export { GenericNotFoundPage };
