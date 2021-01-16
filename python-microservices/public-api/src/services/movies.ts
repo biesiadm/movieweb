1e7f711817901d307af6b3e07aafee19ff280237
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-01-16 11:21:02 +0100

    Move `poster_url` and `slug` to public API (#6)

--- python-microservices/public-api/src/services/movies.ts
+++ python-microservices/public-api/src/services/movies.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import express from 'express';
+import slugify from 'slugify';
 import { axiosInstance } from './../config';
 import { Movie, MoviesApiFactory } from './../api/movie/api';
 import { Configuration } from './../api/movie/configuration';
@@ -17,27 +18,47 @@ const api = MoviesApiFactory(
  *   schemas:
  *     Movie:
  *       type: "object"
+ *       required:
+ *         - id
+ *         - title
+ *         - slug
+ *         - poster_url
  *       properties:
- *         title:
- *           title: "Title"
- *           type: "string"
- *         director:
- *           title: "Director"
- *           type: "string"
- *         year:
- *           title: "Year"
- *           type: "integer"
- *         country:
- *           title: "Country"
- *           type: "string"
- *         category:
- *           title: "Category"
- *           type: "string"
  *         id:
- *           title: "Id"
  *           type: "string"
  *           format: "uuid"
+ *         title:
+ *           type: "string"
+ *         slug:
+ *           type: "string"
+ *         poster_url:
+ *           type: "string"
+ *           format: "url"
+ *         director:
+ *           type: "string"
+ *         year:
+ *           type: "integer"
+ *         country:
+ *           type: "string"
+ *         category:
+ *           type: "string"
  */
+interface PublicMovie extends Movie {
+
+    /**
+     *
+     * @type {string}
+     * @memberof Movie
+     */
+    slug: string;
+
+    /**
+     *
+     * @type {string}
+     * @memberof Movie
+     */
+    poster_url: string;
+}
 
 /**
  * @swagger
@@ -64,6 +85,19 @@ const api = MoviesApiFactory(
 router.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
     api.readMoviesMoviesGet()
         .then((axiosResponse: AxiosResponse<Movie[]>) => {
+            axiosResponse.data = axiosResponse.data.map((movie: Movie) => {
+                let result: Partial<PublicMovie> = movie;
+                result.poster_url = 'https://fwcdn.pl/fpo/10/39/1039/7517880.3.jpg';
+                result.slug = slugify(movie.title, {
+                    lower: true,
+                    strict: true,
+                    locale: 'en'
+                });
+                return <PublicMovie>result;
+            });
+            return <AxiosResponse<PublicMovie[]>>axiosResponse;
+        })
+        .then((axiosResponse: AxiosResponse<PublicMovie[]>) => {
             res.status(axiosResponse.status).json(axiosResponse.data);
             return next();
         })
--- python-microservices/webapp/src/components/MovieCard.tsx
+++ python-microservices/webapp/src/components/MovieCard.tsx
@@ -1,28 +1,57 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { Movie } from '../api/public/api'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Movie } from '../api/public/api';
+import { BASE_URL } from '../config';
 
 type Props = {
   movie: Movie
 }
 
-type EmptyState = Record<string, never>
+type State = {
+  details_url: URL
+}
 
-class MovieCard extends Component<Props, EmptyState> {
+class MovieCard extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.computeState.bind(this);
+    this.handleChange.bind(this);
+    this.render.bind(this);
+
+    this.state = this.computeState();
+  }
+
+  computeState = (): State => {
+    const movie = this.props.movie;
+    const details_url = new URL(BASE_URL + '/movies/' + movie.slug + '-' + movie.id);
+    return {
+      details_url: details_url
+    };
+  }
+
+  // Update details URL when props change
+  handleChange = () => {
+    this.setState(this.computeState());
+  };
+
   render(): React.ReactNode {
+    const movie = this.props.movie;
+    const url = this.state.details_url.pathname;
     return <div className="card shadow-sm">
             <div className="row g-0">
               <div className="col-md-4">
-                <Link to="/movies/slug-here">
-                  <img src="https://fwcdn.pl/fpo/10/39/1039/7517880.3.jpg" className="card-img-top" alt="{this.props.movie.title} poster" />
+                <Link to={url}>
+                  <img src={movie.poster_url} className="card-img-top" alt="{movie.title} poster" />
                 </Link>
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{this.props.movie.title}</h5>
-                  <h6 className="card-subtitle mb-2 text-muted">{this.props.movie.director}</h6>
-                  <p className="card-text text-muted small mb-0">{this.props.movie.country}, {this.props.movie.year}</p>
-                  <p className="card-text text-muted small"><Link to="/movies/slug-here">Details</Link></p>
+                  <h5 className="card-title">{movie.title}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{movie.director}</h6>
+                  <p className="card-text text-muted small mb-0">{movie.country}, {movie.year}</p>
+                  <p className="card-text text-muted small"><Link to={url}>Details</Link></p>
                 </div>
               </div>
             </div>
