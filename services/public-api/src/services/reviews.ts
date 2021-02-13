8d32dcfed43114ed390a2a51b94a2e781f5b99fc
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-02-13 17:38:42 +0100

    Update reviews client in public-api

--- services/public-api/src/services/reviews.ts
+++ services/public-api/src/services/reviews.ts
@@ -115,13 +115,7 @@ interface PublicReview extends Review {
 router.get("/", handlePagination);
 router.get("/", handleReviewSorting);
 router.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
-
-    // TODO(biesiadm): Handle /reviews/
-    // TODO(biesiadm): Handle sorting in reviews API
-    // TODO(kantoniak): Load reviews for all movies
-    // This ID changes with every restart of reviews service
-    const movie_id = 'fab9d53d-d9ee-4f53-a9ec-32f0067682f2';
-    reviewsApi.readReviewsApiReviewsMovieMovieIdReviewsGet(movie_id, req.pagination!.skip, req.pagination!.limit,
+    reviewsApi.readAllReviewsApiReviewsReviewsGet(req.pagination!.skip, req.pagination!.limit,
         req.sorting?.by, req.sorting?.dir)
         .then((axiosResponse: AxiosResponse<Review[]>) => {
             axiosResponse.data = axiosResponse.data.map((review: Review) => <PublicReview>review);
@@ -228,7 +222,7 @@ movieRouter.get("/", (req: express.Request, res: express.Response, next: express
     // TODO(biesiadm): Handle sorting in reviews API
     // TODO(biesiadm): Can we tame these OpenAPI operation names? It's just an extra declaration: https://fastapi.tiangolo.com/advanced/path-operation-advanced-configuration/#openapi-operationid
     const movie_id: string = req.params.id;
-    reviewsApi.readReviewsApiReviewsMovieMovieIdReviewsGet(movie_id, req.pagination!.skip, req.pagination!.limit,
+    reviewsApi.readMovieReviewsApiReviewsMovieMovieIdReviewsGet(movie_id, req.pagination!.skip, req.pagination!.limit,
         req.sorting?.by, req.sorting?.dir)
         .then((axiosResponse: AxiosResponse<Review[]>) => {
             axiosResponse.data = axiosResponse.data.map((review: Review) => <PublicReview>review);
@@ -308,7 +302,7 @@ userRouter.get("/", (req: express.Request, res: express.Response, next: express.
 
     // TODO(biesiadm): Handle sorting in reviews API
     const user_id: string = req.params.id;
-    reviewsApi.readReviewsApiReviewsUserUserIdReviewsGet(user_id, req.pagination!.skip, req.pagination!.limit,
+    reviewsApi.readUserReviewsApiReviewsUserUserIdReviewsGet(user_id, req.pagination!.skip, req.pagination!.limit,
         req.sorting?.by, req.sorting?.dir)
         .then((axiosResponse: AxiosResponse<Review[]>) => {
             axiosResponse.data = axiosResponse.data.map((review: Review) => <PublicReview>review);
