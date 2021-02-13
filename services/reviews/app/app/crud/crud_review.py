df046f36478de6c6746593400c6f62fb98935bea
Mateusz Biesiadowski <mateusz.biesiadowski@gmail.com>
2021-02-13 16:27:08 +0100

    Removing reviews and count + avg for user

--- services/reviews/app/app/crud/crud_review.py
+++ services/reviews/app/app/crud/crud_review.py
@@ -71,5 +71,11 @@ class CRUDReview(CRUDBase[Review, ReviewCreate, ReviewUpdate]):
     def get_count_by_movie(self, db: Session, *, movie_id: UUID):
         return db.query(self.model).with_entities(func.count(Review.rating)).filter(Review.movie_id == movie_id).first()
 
+    def get_average_by_user(self, db: Session, *, user_id: UUID):
+        return db.query(self.model).with_entities(func.avg(Review.rating)).filter(Review.user_id == user_id).first()
+
+    def get_count_by_user(self, db: Session, *, user_id: UUID):
+        return db.query(self.model).with_entities(func.count(Review.rating)).filter(Review.user_id == user_id).first()
+
 
 review = CRUDReview(Review)
